413ac64d77827d3ca6e445fd32825feb14d5cf55
johnbain881 <60230743+johnbain881@users.noreply.github.com>
2020-02-15 11:10:51 -0600

    Added about page and gray background

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask import request
+from flask import Response
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 # SERVE WEB PAGES
+@app.route('/style.css', methods=['GET'])
+def styleCSS():
+    css = open("static_content/style.css", "r").read()
+    return Response(css, mimetype='text/css')
 
 @app.route('/', methods=['GET'])
 @app.route('/index.html', methods=['GET'])
 def indexHtml():
     return open("static_content/index.html", "r").read()
+
 
 @app.route('/index.js', methods=['GET'])
 def indexJs():
@@ -30,6 +36,52 @@ def add():
     num1 = int(request.form["first_num"])
     num2 = int(request.form["second_num"])
     return str(num1 + num2)
+
+@app.route('/happyprime/', methods=['POST'])
+def happy():
+    num = int(request.form["happy_prime"])
+    if is_prime(num):
+        if happy_prime(num):
+            return str(num) + " is a Happy Prime Number"
+        else:
+            return str(num) + " is not a Happy Prime Number"
+    else:
+        return str(num) + " is not a Happy Prime Number"
+
+@app.route('/about/', methods=['GET'])
+def about():
+    return """{
+    "solution":"about",
+    "value":"2"
+}"""
+
+
+def happy_prime(input):
+    num = 0
+    if input == 1:
+        return True
+    if input == 4:
+        return False
+    else:
+        place = str(input)
+        for x in range(0, len(place)):
+            num += int(place[x])**2
+        return happy_prime(num)
+
+
+def is_prime(input):
+    input = abs(int(input))
+
+    if input < 2:
+        return False
+    if input % 2 == 0:
+        return False
+    if not input & 1:
+        return False
+    for x in range(3, int(input**0.5)+1, 2):
+        if input % x == 0:
+            return False
+    return True
 
 # START SERVING
 
